2e0ae6776a0b6435ea3cef59b9b402d1447d1464
Hend Soliman <hendsoliman5@gmail.com>
2025-12-19 01:55:38 +0400

    Initial commit

---
+++ src/main/java/com/dvt/udemy/vertx_starter/MainVerticle.java
@@ -0,0 +1,27 @@
+package com.dvt.udemy.vertx_starter;
+
+import io.vertx.core.Future;
+import io.vertx.core.VerticleBase;
+import io.vertx.core.Vertx;
+
+public class MainVerticle extends VerticleBase {
+
+
+  public static void main(String[] args) {
+    Vertx vetx= Vertx.vertx();
+    vetx.deployVerticle(new MainVerticle()).onSuccess(success -> {
+      System.out.printf("Verticle deployed successfully\n");
+    }).onFailure(err -> { err.printStackTrace(); System.exit(1); });
+
+  }
+  @Override
+  public Future<?> start() {
+    return vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello from Vert.x!");
+    }).listen(8888).onSuccess(http -> {
+      System.out.println("HTTP server started on port 8888");
+    });
+  }
+}
